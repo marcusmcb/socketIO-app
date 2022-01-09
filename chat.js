2bd581a57cb4e3924de03f6e92b823a476862068
Marcus McBride <marcusmcb@gmail.com>
2022-01-09 09:49:00 -0800

    first commit

---
+++ chat.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'))
+
+const expressServer = app.listen(8000)
+const io = socketio(expressServer)
+
+io.on('connection', (socket) => {
+  socket.emit('messageFromServer', { data: 'Welcome to the SocketIO server.' })
+  socket.on('messageToServer', (dataFromClient) => {
+    console.log(dataFromClient)
+  })
+  socket.on('newMessageToServer', (msg) => {
+    io.emit('messageToClients', { text: msg.text })
+    // io.of('/').emit('messageToClients', {text: msg.text})
+  })
+})
+
+io.of('/admin').on('connection', (socket) => {
+  console.log('Admin namespace connected.')
+  io.of('/admin').emit('welcome', 'Welcome to the admin channel.')
+})
